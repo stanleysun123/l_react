9f5f1a113a6a5703a3ccb8cd06128bbd3aa2702b
Stanley.Sun <Stanley.sun@aarnet.edu.au>
2019-11-30 13:07:38 +1100

    add winner logic and alerts

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -44,6 +44,19 @@ class Board extends React.Component {
 
      handleclick(i) {
           const squares = this.state.squares.slice();
+
+
+         if (squares[i]) {
+             alert('Already filled');
+             return;
+         }
+
+         //check winner or filled
+         if (calculateWinner(squares)) {
+             alert('Game finished');
+             return;
+         }
+
           squares[i] = this.state.xIsNext ? 'X': 'O';
           this.setState(
               {squares:squares,
@@ -61,7 +74,16 @@ class Board extends React.Component {
       }
 
     render() {
-        const status = 'Next player: X';
+
+        const winner  = calculateWinner(this.state.squares);
+        let status;
+
+        if (winner){
+           status =  'Winner ' + winner
+        }else {
+           status =  'Next player: ' + (this.state.xIsNext ? 'X' :'O') ;
+        }
+
 
         return (
             <div>
@@ -108,3 +130,22 @@ ReactDOM.render(
 <Game />,
     document.getElementById('root')
 );
+
+ function calculateWinner(squares){
+
+       const winlines = [
+          [0,1,2], [3,4,5], [6,7,8],
+           [0,3,6], [1,4,7],[2,5,8],
+           [0,4,8], [2,4,6],
+       ]
+
+      for (let i=0 ; i < winlines.length; i++){
+          const [a,b,c] =winlines[i]
+          if(squares[a]&& squares[a] === squares[b] && squares[a] ===squares[c] ){
+              return squares[a];
+          }
+      }
+
+       return null;
+
+ }
